0ddfa2891dfbf0ebf41a520c83ced416db68a3ac
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-28 07:40:10 +0600

    Character arrays and pointers part - II

---
+++ Pointer11.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file Pointer11.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Character arrays and pointers part - II
+ * @version 0.1
+ * @date 2022-07-28
+ * @since Thursday; 6:31 AM
+ * @copyright Copyright (c) 2022
+ */
+
+#include <iostream>
+using namespace std;
+/*Character array traverse without extra variable with pointer*/
+void print(const char * name){
+    while(* name != '\0'){
+        cout<<*name;
+        name++;
+    }
+    cout<<endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    system("cls");
+    /*Using const keyword: read only*/
+    const char *name = "YOUSUF";
+    print(name);
+    return 0;
+}
