00afa64f6fbd743e94d70b619b7a036d19d57408
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-08-02 22:06:20 +0600

    Absolute compare call-back function

---
+++ Pointer21.cpp
@@ -0,0 +1,52 @@
+/**
+ * @file Pointer21.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Absolute compare call-back function
+ * @version 0.1
+ * @date 2022-08-02
+ * @since TuesDay; 6:46 PM
+ * @copyright Copyright (c) 2022
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+/*Callback function should compare two integers,
+shuld return 1 if firs element is higer rank,
+0 if elements are equal and -1 if second element has higher rank*/
+/*Call-back function: int compare(int, int)*/
+int absoluteCompare(int firstElement, int secondElement){
+    if(abs(firstElement) > abs(secondElement)){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+}
+/*Bubble sort optimal solution*/
+void bubbleSort(int * A, unsigned int *n, int (*compare)(int, int)){
+    bool swappedStatus = true;
+    for(unsigned int i = 0; i<*n-1 && swappedStatus == true; i++){
+        swappedStatus = false;
+        for(unsigned int j = 0; j<*n - (i+1); j++){
+            if(absoluteCompare(*(A+j) , *(A+j+1)) > 0){
+                swap(*(A+j), *(A+j+1));
+                swappedStatus = true;
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[]){
+    /* code */
+    system("cls");
+    int A[] = {-31, 22, -1, 50, -6 ,70, -4};
+    unsigned int n = sizeof(A) / sizeof(int);
+
+    bubbleSort(A, &n, absoluteCompare);
+    for(unsigned int i = 0; i<n; i++){
+        cout<<"["<<i<<"] = "<<A[i]<<endl;
+    }
+    return 0;
+}
+
